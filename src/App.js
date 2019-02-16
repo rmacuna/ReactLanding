c64edf45de7d654271c642be3744f6b44bed9875
rmacuna <rmacuna@uninorte.edu.co>
2019-02-16 14:36:59 -0500

    Responsive de la primera mitad de la pagina terminado, hay que ajustar los breakpoints para algunas pantallas medianas ya que la imagen de MartinGarrixyCharliePuth estan bloqueando los textos

--- src/App.js
+++ src/App.js
@@ -5,30 +5,41 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import { theme } from './components/AppTheme';
 import { ShadowText, Title, Subtitle, Text } from './components/Typography/Typography';
 import AppContainer from './components/AppContainer';
-
+import CharlieImage from './images/top50.png';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        
         <div className="MainWrapper Linear--purple__background">
-          <AppBar></AppBar>
-          <ShadowText>MUSIC</ShadowText>
-          <Title color="#fff">Sounds Good</Title>
-          <Subtitle color={theme.main.tertiary} fontSize="90px">Sounds Louder</Subtitle>
-          <AppContainer textContainer left="0" right="0" width="510px">
-            <Text margin="16px 0" align="justify"  color="#fff" fontSize="21px">
-              Prepare for a list of incredible music with Loudify Get newest tracks and follow the creations of your favorite artist.
-            </Text>
-          </AppContainer>
-          <ShadowText marginTop="45px">NOW</ShadowText>
-          <Text fontSize="18px" weight="600" margin="12px 0" color={theme.main.secondary}> Turn up the speakers</Text>
-          {/* <Grid fluid>
-            <Row>
-              <Col xs={12}>
-              </Col>
-            </Row>
-          </Grid> */}
+          {/* Background of martin garrix and Charlie Puth */}
+          <div className="MainWrapperBackground"></div> 
+
+          {/* Appbar  */}
+          <AppBar />
+          
+          {/* Main Container that flex two items , the right section that have a image of Martin Garrix and Charlie Puth and left section contains texts and descriptions */}
+          <div className="MainContainer">
+
+            <div className="TextSection">
+              <ShadowText>MUSIC</ShadowText>
+              <Title color="#fff">Sounds Good</Title>
+              <Subtitle color={theme.main.tertiary} fontSize="90px">Sounds Louder</Subtitle>
+              <AppContainer textContainer left="0" right="0" width="510px">
+                <Text margin="16px 0" align="justify" color="#fff" fontSize="21px">
+                  Prepare for a list of incredible music with Loudify Get newest tracks and follow the creations of your favorite artist.
+                </Text>
+              </AppContainer>
+              <ShadowText marginTop="45px">NOW</ShadowText>
+              <Text className="Cursor" fontSize="18px" weight="600" margin="12px 0" color={theme.main.secondary}> Turn up the speakers</Text>
+            </div>
+         
+            <div className="ImageSection">
+              <img className="hideOnPhone" src={CharlieImage} alt="MartinGarrix, Charlie Puth"/>
+            </div>
+          </div>
+
         </div>
         <div className="SecondWrapper">
 
--- src/components/Typography/Typography.jsx
+++ src/components/Typography/Typography.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { theme } from '../AppTheme';
-import styled, { css } from 'styled-components';
+import { theme, Media } from '../AppTheme';
+import styled from 'styled-components';
 
 const StyledTitle = styled.h1`
-    font-weight: ${props => props.light ? '600' : '700'};
+    font-weight: ${props => props.weight ? props.weight : '700'};
     font-size: ${props => props.fontSize ? props.fontSize : '65px'};
     padding: ${props => props.padding ? props.padding : '0'};
     margin: 0;
     color: ${props => props.color ? props.color : '#13003c'};
+
+    ${Media.phone`
+        font-size: 40px;
+    `}
 `
 const StyledSubtitle = styled.h2`
     margin: 0;
-    font-weight: ${props => props.light ? '600' : '700'};
+    font-weight: ${props => props.weight ? props.weight : '700'};
     font-size: ${props => props.fontSize ? props.fontSize : '65px'};
     padding: ${props => props.padding ? props.padding : '0'};
     color: ${props => props.color ? props.color : '#13003c'};
+
+    ${Media.phone`
+        font-size: 55px;
+    `}
 `
 
 const Shadow = styled.h1`
@@ -25,6 +33,12 @@ const Shadow = styled.h1`
     margin: 0;
     margin-top: ${props => props.marginTop ? props.marginTop : '21px'};
     text-align: ${props => props.align ? props.align : 'inherit'};
+
+
+    ${Media.phone`
+        font-size: 35px;
+    `}
+    
 `
 
 
@@ -34,8 +48,12 @@ const StyledText = styled.p`
     padding: ${props => props.padding ? props.padding : '0'};
     text-align: ${props => props.align ? props.align :  'auto'};
     color: ${props => props.color ? props.color : '#13003c'};
-    
     margin: ${props => props.margin ? props.margin : '0'};
+
+
+    ${Media.phone`
+        font-size: 16px;
+    `}
 `
 
 export const Text = (props) => {
