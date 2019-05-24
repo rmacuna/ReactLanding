119fed28de06bace6bbc253297d0e5df15364ffc
rmacuna <rmacuna@uninorte.edu.co>
2019-05-24 09:51:05 -0500

    Agregado el sdk de firebase al proyecto y hosteada la app en firebase hosting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import AppRouter from './app.routing';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBi-egFQ87ZKj_l2QlEzahtXCGHXrjTXWQ",
+    authDomain: "loudify.firebaseapp.com",
+    databaseURL: "https://loudify.firebaseio.com",
+    projectId: "loudify",
+    storageBucket: "loudify.appspot.com",
+    messagingSenderId: "740690972444",
+    appId: "1:740690972444:web:4887542850dd9e62"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
